ad62e88d8ddce7e8cd260013b2fddaa16bb90b1e
anouther <155570107+anouther@users.noreply.github.com>
2024-12-12 18:52:58 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@
           } else {
             // Setelah teks utama selesai, tambahkan subtitle kecil
             const subtitle = document.createElement('p');
-            subtitle.innerText = "cuz man do also deserve flowers, remember? hope you like itt~ ";
+            subtitle.innerText = "gavan aku sayang banget sama kamu, be happy yaa";
             subtitle.style.fontSize = '20px'; // Ukuran font lebih kecil
             subtitle.style.marginTop = '2px'; // Spasi antara teks utama dan subtitle
             subtitle.style.color = '#fff'; // Warna abu-abu untuk teks subtitle
